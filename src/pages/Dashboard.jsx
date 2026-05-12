27bac7f78a22f991c7d89c12cc29b750fd843582
Votre Nom <votre@email.com>
2026-05-12 18:56:36 +0200

    Fix import image Guy Roux

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import guyRoux from '../assets/guy-roux.png'
+import guyRoux from '../assets/guyroux.png'
 import { useNavigate } from 'react-router-dom'
 import {
   ChevronLeft, ChevronRight, Printer,
